28670a4284c598c4302ef7ae9e9a752cf6128cb2
Dmitry <dmitrychinyaev93@yandex.ru>
2023-06-15 11:37:19 +0300

    Validation added

--- src/main/java/ru/dmitrychinyaev/savenewsbot/entity/Message.java
+++ src/main/java/ru/dmitrychinyaev/savenewsbot/entity/Message.java
@@ -3,15 +3,14 @@ package ru.dmitrychinyaev.savenewsbot.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
 public class Message {
     private Integer date;
-    @Min(10)
-    //TODO Посмотреть значения и что возвращается если меньше символов
     private String authorName;
+    @Size(min = 10, max = 200, message = "Сообщение должно быть состоять из более 10 символов, но менее 200")
     private String text;
 
     @Override
